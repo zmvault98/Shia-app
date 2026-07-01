8047f9391328d85cbc5e0b64675b0210c6dad279
zmvault98 <zm.vault.1@gmail.com>
2026-07-01 23:11:21 +0500

    Add files via upload

---
+++ Sahifa.js
@@ -0,0 +1,544 @@
+const sahifa = [
+
+{
+id:101,
+english:"In Praise of Allah",
+urdu:"خداوند عالم کی حمد و ثناء",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 1"
+},
+
+{
+id:102,
+english:"Blessings upon Prophet Muhammad (ﷺ)",
+urdu:"پیغمبر اسلام ﷺ پر درود و سلام",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 2"
+},
+
+{
+id:103,
+english:"Blessings upon the Bearers of the Throne",
+urdu:"حاملانِ عرش پر درود و سلام",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 3"
+},
+
+{
+id:104,
+english:"Blessings upon the Confirmers of the Messengers",
+urdu:"انبیاء علیہم السلام پر ایمان لانے والوں پر درود و سلام",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 4"
+},
+
+{
+id:105,
+english:"For Himself and His Friends",
+urdu:"اپنے اور اپنے دوستوں کے لئے دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 5"
+},
+
+{
+id:106,
+english:"Morning and Evening",
+urdu:"صبح و شام کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 6"
+},
+
+{
+id:107,
+english:"In Times of Worry",
+urdu:"مشکلات اور پریشانیوں کے وقت کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 7"
+},
+
+{
+id:108,
+english:"Seeking Refuge",
+urdu:"پناہ کے سلسلے کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 8"
+},
+
+{
+id:109,
+english:"Seeking Forgiveness",
+urdu:"طلبِ مغفرت کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 9"
+},
+
+{
+id:110,
+english:"Seeking Protection",
+urdu:"پناہ طلب کرنے کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 10"
+},
+
+{
+id:111,
+english:"For Good Outcomes",
+urdu:"انجام بخیر ہونے کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 11"
+},
+
+{
+id:112,
+english:"Confession and Repentance",
+urdu:"اعترافِ گناہ اور طلبِ توبہ کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 12"
+},
+
+{
+id:113,
+english:"Seeking Needs",
+urdu:"طلبِ حاجات کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 13"
+},
+
+{
+id:114,
+english:"Seeking Justice",
+urdu:"دادخواہی کی بابت کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 14"
+},
+
+{
+id:115,
+english:"When Sick",
+urdu:"مرض کے دفعیہ کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 15"
+},
+{
+id:116,
+english:"Seeking Pardon",
+urdu:"عذر و عفوِ تقصیر کے سلسلے میں دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 16"
+},
+
+{
+id:117,
+english:"Against Satan",
+urdu:"شر شیطان کے دفعیہ کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 17"
+},
+
+{
+id:118,
+english:"Repelling Afflictions",
+urdu:"رفعِ بلیات کے سلسلے میں دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 18"
+},
+
+{
+id:119,
+english:"For Rain",
+urdu:"طلبِ باراں کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 19"
+},
+
+{
+id:120,
+english:"Noble Moral Traits",
+urdu:"پاکیزہ اخلاق سے آراستگی کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 20"
+},
+
+{
+id:121,
+english:"In Sorrow",
+urdu:"رنج و اندوہ کے موقع کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 21"
+},
+
+{
+id:122,
+english:"In Hardship",
+urdu:"شدت و سختی کے وقت کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 22"
+},
+
+{
+id:123,
+english:"For Well-Being",
+urdu:"طلبِ عافیت کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 23"
+},
+
+{
+id:124,
+english:"For Parents",
+urdu:"والدین کے حق میں دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 24"
+},
+
+{
+id:125,
+english:"For Children",
+urdu:"اولاد کے حق میں حضرت کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 25"
+},
+
+{
+id:126,
+english:"For Neighbours and Friends",
+urdu:"ہمسایوں اور دوستوں کے لئے دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 26"
+},
+
+{
+id:127,
+english:"For the Frontier Guards",
+urdu:"سرحدوں کے محافظوں کے لئے دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 27"
+},
+
+{
+id:128,
+english:"Supplication before Allah",
+urdu:"اللہ سے تضرع و زاری کے سلسلے میں دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 28"
+},
+
+{
+id:129,
+english:"When Provision is Limited",
+urdu:"تنگیِ رزق کے موقع پر پڑھنے کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 29"
+},
+
+{
+id:130,
+english:"Repaying Debts",
+urdu:"ادائے قرض کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 30"
+},
+{
+id:131,
+english:"Repentance",
+urdu:"دعائے توبہ",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 31"
+},
+
+{
+id:132,
+english:"After the Night Prayer",
+urdu:"نمازِ شب کے بعد کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 32"
+},
+
+{
+id:133,
+english:"Seeking Guidance (Istikhara)",
+urdu:"دعائے استخارہ",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 33"
+},
+
+{
+id:134,
+english:"Protection from Disgrace",
+urdu:"گناہوں کی رسوائی سے بچنے کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 34"
+},
+
+{
+id:135,
+english:"Contentment with Allah's Decree",
+urdu:"رضائے الٰہی پر خوش رہنے کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 35"
+},
+
+{
+id:136,
+english:"Upon Seeing Clouds, Lightning and Thunder",
+urdu:"بادل اور بجلی کو دیکھتے اور رعد کی آواز",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 36"
+},
+
+{
+id:137,
+english:"In Thankfulness",
+urdu:"ادائے شکر میں کوتاہی کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 37"
+},
+
+{
+id:138,
+english:"Seeking Forgiveness and Mercy",
+urdu:"عذر و مغفرت کے سلسلے میں دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 38"
+},
+
+{
+id:139,
+english:"Seeking Pardon and Mercy",
+urdu:"طلبِ عفو و رحمت کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 39"
+},
+
+{
+id:140,
+english:"Remembering Death",
+urdu:"موت کو یاد کرنے کے وقت کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 40"
+},
+
+{
+id:141,
+english:"Concealment and Protection",
+urdu:"پردہ پوشی و نگہداشت کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 41"
+},
+
+{
+id:142,
+english:"Upon Completing the Qur'an",
+urdu:"قرآن ختم کرنے کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 42"
+},
+
+{
+id:143,
+english:"Upon Sighting the New Moon",
+urdu:"رویتِ ہلال کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 43"
+},
+
+{
+id:144,
+english:"Welcoming Ramadan",
+urdu:"ماہ رمضان کے استقبال کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 44"
+},
+
+{
+id:145,
+english:"Farewell to Ramadan",
+urdu:"ماہ رمضان کو الوداع کرنے کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 45"
+},
+{
+id:146,
+english:"For Eid and Friday",
+urdu:"عیدین اور جمعہ کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 46"
+},
+
+{
+id:147,
+english:"On the Day of Arafah",
+urdu:"روز عرفہ کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 47"
+},
+
+{
+id:148,
+english:"For Eid al-Adha and Friday",
+urdu:"عید قربان اور جمعہ کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 48"
+},
+
+{
+id:149,
+english:"Against the Enemy's Plot",
+urdu:"دشمن کے مکر و فریب کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 49"
+},
+
+{
+id:150,
+english:"In Fear of Allah",
+urdu:"خوفِ الٰہی کے سلسلے میں حضرت کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 50"
+},
+
+{
+id:151,
+english:"Humility before Allah",
+urdu:"عجز و زاری کے سلسلے میں دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 51"
+},
+
+{
+id:152,
+english:"Earnest Supplication",
+urdu:"تضرع و الحاح کے سلسلے میں دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 52"
+},
+
+{
+id:153,
+english:"Humbleness before Allah",
+urdu:"عجز و فروتنی کے سلسلے میں دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 53"
+},
+
+{
+id:154,
+english:"Removal of Sorrow",
+urdu:"رنج و اندوہ کے دور ہونے کی دعا",
+bismillah:"بِسْمِ اللَّهِ الرَّحْمٰنِ الرَّحِيمِ",
+arabic:"gg",
+translation:"gg",
+reference:"صحیفہ سجادیہ<br>دعا 54"
+},
+
+
+
+
+
+];
